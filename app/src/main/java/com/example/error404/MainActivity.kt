c5824f640c2721699ebb9cd68c9ff2b787f6be61
JLK <20ka208@esisalama.org>
2023-06-14 10:05:35 +0200

    add button

--- app/src/main/java/com/example/error404/MainActivity.kt
+++ app/src/main/java/com/example/error404/MainActivity.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -14,7 +20,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.error404.ui.theme.Error404Theme
 import java.lang.Error
@@ -55,7 +63,24 @@ fun errorMessage(error: String, message: String, modifier: Modifier = Modifier){
             fontSize = 30.sp,
             style = MaterialTheme.typography.bodyLarge
         )
+    }
+    Errobutton()
+}
 
+@Composable
+fun Errobutton(){
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally,){
+        Button(
+            {}, Modifier.padding(80.dp), shape = RoundedCornerShape(20.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color (63,123,255))
+        ) {
+            Text(text = "GO HOME.",
+                color = Color.White
+
+            )
+        }
     }
 }
 
